60fd9554284888d922f9b1621a76a273583a02a9
Tanishq Mor <Tanishq.Mor@gmail.com>
2021-05-07 14:40:31 -0700

    Analysis.py now writes the data to csv

--- Analysis.py
+++ Analysis.py
@@ -5,16 +5,14 @@ from beeprint import pp
 import pandas as pd
 
 def get_data(hotspot):
-    # Url for the helium api
     url='https://api.helium.io/v1/hotspots/' + hotspot + '/activity'
     # using the request library to get the data into a json
     response = requests.get(url)
     output = response.json()
     # cursor needded to go to next pages 
     cursor = output['cursor']
-    # create a dic to store the values 
-    my_dict = {}
-    my_dict['data'] = []
+    # create a tableList to store the values 
+    tableList = []
     # for loop to go throught the first page and print the data 
     for i in output['data']:
         if (i['type'] == "poc_receipts_v1"):
@@ -24,14 +22,15 @@ def get_data(hotspot):
             city = i['path'][0]['geocode']['short_city']
             witnesses = len(i['path'][0]['witnesses'])
             # insert the values into a dict 
-            my_dict['data'].append({
+            allthedata = {
                 'TimeStamp' : timestamp,
                 'Challengee' : challengee,
                 'Street'    : street,
                 'City'      : city,
                 'Witnesses' : witnesses
-                
-            })
+            }
+            tableList.append(allthedata)
+            
             
     # for loop to go throught and print the data after using cursor to go to next page
     url=url+'?cursor='+cursor
@@ -45,20 +44,18 @@ def get_data(hotspot):
             city = i['path'][0]['geocode']['short_city']
             witnesses = len(i['path'][0]['witnesses'])
             # insert the values into a dict
-            my_dict['data'].append({
+            alltheData = {
                 'TimeStamp' : timestamp,
                 'Challengee' : challengee,
                 'Street'    : street,
                 'City'      : city,
                 'Witnesses' : witnesses
-                
-            })
-    # write the data into a json
-    # with open('data.json', 'w') as outfile:
-    #     json.dump(my_dict, outfile)
-    return my_dict
+            }
+            tableList.append(alltheData)
+    
+    return tableList
 
+# Update as needed 
 data = get_data('112XTwrpTBHjg4M1DWsLTcqsfJVZCPCYW2vNPJV7cZkpRg3JiKEg')
 df = pd.DataFrame(data)
 df.to_csv('hotspotData.csv')
-df.to_excel('HotspotData.xlsx')
