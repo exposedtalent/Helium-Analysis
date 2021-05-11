0a4b7f9034ad5e265ce2d8fab4030541e8246d22
Tanishq Mor <Tanishq.Mor@gmail.com>
2021-05-11 13:20:36 -0700

    printing the reward scale for each

--- Analysis.py
+++ Analysis.py
@@ -66,36 +66,36 @@ def summary(file):
     col_list = ["Witnesses"]
     df = pd.read_csv(file, usecols=col_list)
     data = df.values
-    
+    # Looks at only the Reward Scale col and store it in a list
     col_list2 = ["reward_scale"]
     df2 = pd.read_csv(file, usecols=col_list2)
     rewardScale = df2.values
-    flat_list = list(np.concatenate(rewardScale).flat)
-    avg = sum(flat_list)/ len(flat_list)
+    rs = list(np.concatenate(rewardScale).flat)
+
+    # Looks at only the Challengee col and store it in a list
+    new_col3 = ["Challengee"]
+    df2 = pd.read_csv(file, usecols=new_col3)
+    Challengee = df2.values
+
+    
     # using the numpy lib we find the number of times this router has seen 1,2,3,4,5 or more witnesses for a challenge
     occurOne = np.count_nonzero(data == 1)  
     occurTwo = np.count_nonzero(data == 2)
     occurThree = np.count_nonzero(data == 3)
     occurFour = np.count_nonzero(data == 4)
-    occurFive = np.count_nonzero(data == 5)
-    occurMore = np.count_nonzero(data > 5)
-    onetofive = occurOne + occurTwo + occurThree + occurFour + occurFive
+    occurMore = np.count_nonzero(data >= 5)
+    onetofour = occurOne + occurTwo + occurThree + occurFour 
 
     print(
-        "1 Witnesses : ", occurOne,"times\n" 
-        "2 Witnesses : ", occurTwo,"times\n"
-        "3 Witnesses : ", occurThree,"times\n"
-        "4 Witnesses : ", occurFour,"times\n"
-        "5 Witnesses : ", occurFive,"times\n"
-        "More than 5 Witnesses :", occurMore, "times\n\n"
-        "Total Witnesses from 1-5 : ", onetofive, "times\n"
-        "Total Witnesses : ", onetofive + occurMore, "\n"
-        "Average Reward Scale : ", round(avg,3), "\n\n"
+        "1 Witnesses : ", occurOne, "\tAVG Reward Scale :", round(avg(rs[0:occurOne]),3),"\n", 
+        "2 Witnesses : ", occurTwo, "\tAVG Reward Scale :", round(avg(rs[occurOne:occurOne+occurTwo]),3),"\n",
+        "3 Witnesses : ", occurThree, "\tAVG Reward Scale :", round(avg(rs[occurOne+occurTwo:occurOne+occurTwo+occurThree]),3),"\n",
+        "4 Witnesses : ", occurFour, "\tAVG Reward Scale :", round(avg(rs[occurOne+occurTwo+occurThree:occurOne+occurTwo+occurThree+occurFour]),3),"\n",
+        "More than 5 Witnesses :", occurMore, "\n\n",
+        "Total Witnesses from 1-5 : ", onetofour, "\n",
+        "Total Witnesses : ", onetofour + occurMore, "\n",
         )
-    # Detailed summary 
-    new_col_list = ["Challengee"]
-    df2 = pd.read_csv(file, usecols=new_col_list)
-    Challengee = df2
+    
     
     print(
         "========================= Names of the Hotspots =========================\n "
@@ -103,7 +103,6 @@ def summary(file):
         "Rounters with 2 witnesses :\n", remove_dup(Challengee[occurOne:occurOne+occurTwo].values), "\n",
         "Rounter with 3 witnesses :\n", remove_dup(Challengee[occurOne+occurTwo:occurOne+occurTwo+occurThree].values), "\n",
         "Rounter with 4 witnesses :\n", remove_dup(Challengee[occurOne+occurTwo+occurThree:occurOne+occurTwo+occurThree+occurFour].values), "\n",
-        "Rounter with 5 witnesses :\n", remove_dup(Challengee[occurOne+occurTwo+occurThree+occurFour:occurOne+occurTwo+occurThree+occurFour+occurFive].values), "\n"
     )
 
 # Function to remove the duplicates in the hotspots name 
@@ -119,18 +118,21 @@ def sortCSV(file):
     df = pd.read_csv(file)
     sorted_df = df.sort_values(by=["Witnesses"], ascending=True)
     sorted_df.to_csv('Sorted_hotspotData.csv', index=False)
+    
+def avg(rs):
+    return sum(rs) / len(rs)
 
 # List of the hotspots names       
 hotspots = [
-            #'112XTwrpTBHjg4M1DWsLTcqsfJVZCPCYW2vNPJV7cZkpRg3JiKEg',
-            #'112Cggcbje3yS4a1YpfyVNt1B2DTYNqiFjwaNEvfJp6fhc8UPuLc',
-            #'112SDjb928fBrnhzLLLif1ZNowE9E8VYfkHLoQTUoUQtuijpaPVd',
-            # '112na4aZ1XZsFFtAwUxtEfvn1kkP37yQ8zaTVvYBBEfkMEUkyzhx',
+             '112XTwrpTBHjg4M1DWsLTcqsfJVZCPCYW2vNPJV7cZkpRg3JiKEg',
+            # '112Cggcbje3yS4a1YpfyVNt1B2DTYNqiFjwaNEvfJp6fhc8UPuLc',
+            # '112SDjb928fBrnhzLLLif1ZNowE9E8VYfkHLoQTUoUQtuijpaPVd',
+            #'112na4aZ1XZsFFtAwUxtEfvn1kkP37yQ8zaTVvYBBEfkMEUkyzhx',
             
             # '11H8cjxUtx9WzCxPkbVq3AKzSYh7Wo5yWnPXLrf8eygiKt6hHVP',
             # '11c4pxUfwby5rtz2PtRm4oxmndc8WAcQg5BxT7CNpU56hHqvp9h',
             # '112KHUoQtauKc7hx2yDceHV1Q2X9DsCtdMeoK28gZMPJvHHLrAQz',
-            '111MtVFr98Qs7Bs1u6CaVQFF2CjqJ83sLfxP1BPsAyR5h4Qa77A'
+            # '111MtVFr98Qs7Bs1u6CaVQFF2CjqJ83sLfxP1BPsAyR5h4Qa77A'
             
             ]
 # This is the main program
