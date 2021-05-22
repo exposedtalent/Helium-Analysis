6db607d5737c35e79f35dff4244630dfc3829994
Tanishq Mor <Tanishq.Mor@gmail.com>
2021-05-22 14:59:40 -0700

    Working AWS file

--- Main-Files/Rewards.py
+++ Main-Files/Rewards.py
@@ -1,13 +1,55 @@
-# This file needs to read from the helium API and print out the 30 day and 24 hours rewards for the hotspot
-# need host name, adreres  of the hotspot, name of th hotspot into a cvs 
-# cognito for aws
 import requests
 import pandas as pd
 import numpy as np
 import json
+from json2html import *
+
 
 rewardList = []
-# Function to get the rewards for the hotspots 
+
+def lambda_handler(event, context):
+
+    # This is to figure out the time
+    twentyfourHour = '-2%20day'
+    thirtyDays = '-30%20day'
+
+    # This is for getting the Hotspot Addr fromt the csv file
+    col_list = ["Hopstop Addr"]
+    df = pd.read_csv('HeliumData.csv', usecols=col_list)
+    data = df.values
+    addr = list(np.concatenate(data).flat)
+    
+    # Get the Host's Name from csv
+    col_list = ["Host Name"]
+    df = pd.read_csv('HeliumData.csv', usecols=col_list)
+    data = df.values
+    hostName = list(np.concatenate(data).flat)
+    
+    # Get the Hotspot Name in a list
+    col_list2 = ["Hotspot Name"]
+    df = pd.read_csv('HeliumData.csv', usecols=col_list2)
+    data = df.values
+    hotspotName = list(np.concatenate(data).flat)
+    
+    # This is for getting the Hotspot Addr fromt the csv file
+    col_list = ["Account Addr"]
+    df = pd.read_csv('HeliumData.csv', usecols=col_list)
+    data = df.values
+    accAddr = list(np.concatenate(data).flat)
+    
+    rewardList = get_rewards(addr, twentyfourHour, thirtyDays, hostName, hotspotName, accAddr)
+    
+    # put the result into a json
+    jsonList = json.dumps(rewardList, indent = 4)
+    # datajson = json2html.convert(json = jsonList)
+    # change to this for aws Lambda instead of print
+    return{
+        'statusCode' : 200,
+        'body': jsonList
+    }
+    
+    
+
 def get_rewards(hotspot, twentyfourHour, thirtyDays, hostName, hotspotName, accAddr):
     total24hrs = []
     total30days = []
@@ -56,74 +98,33 @@ def get_rewards(hotspot, twentyfourHour, thirtyDays, hostName, hotspotName, accA
     for i in range(len(hotspot)):
         # Put eveything into a dict
         rewardDict = {
-            'Hotspot Owner' : hostName[i],
-            'Hotspot Address' : hotspot[i],
-            'Hotspot Name' : hotspotName[i],
-            'Hotspot 24H HNT' : round(total24hrs[i], 2),
-            'Hotspot 24H USD' : round(total24hrs[i] * price, 2),
-            '24H Change' : rewardChange[i],
-            'Hotspot 30D HNT' : round(total30days[i], 2),
-            'Hotspot 30D USD' : round(total30days[i] * price, 2),
-            'Wallet Balance' : round(balanceList[i] / 100000000 , 2),
-            'Wallet Balance USD' : round((balanceList[i] / 100000000) * price , 2),
+            'Hotspot_Owner' : hostName[i],
+            'Hotspot_Address' : hotspot[i],
+            'Hotspot_Name' : hotspotName[i],
+            'Hotspot_24H_HNT' : round(total24hrs[i], 2),
+            'Hotspot_24H_USD' : round(total24hrs[i] * price, 2),
+            'Change_24H' : rewardChange[i],
+            'Hotspot_30D_HNT' : round(total30days[i], 2),
+            'Hotspot_30D_USD' : round(total30days[i] * price, 2),
+            'Wallet_Balance' : round(balanceList[i] / 100000000 , 2),
+            'Wallet_Balance_USD' : round((balanceList[i] / 100000000) * price , 2),
             
         }
         rewardList.append(rewardDict)
     
     balanceDict = {
-        'Hotspots 24H HNT' : round(sum(total24hrs), 2),
-        'Hotspots 24H USD' : round(sum(total24hrs) * price, 2),
-        'Hotspots 30D HNT' : round(sum(total30days), 2),
-        'Hotspots 30D USD' : round(sum(total30days) * price, 2),
-        'Total HNT' : round(bal, 2),
-        'Total USD' : round(usdBal, 2),
+        'Hotspots_24H_HNT' : round(sum(total24hrs), 2),
+        'Hotspots_24H_USD' : round(sum(total24hrs) * price, 2),
+        'Hotspots_30D_HNT' : round(sum(total30days), 2),
+        'Hotspots_30D_USD' : round(sum(total30days) * price, 2),
+        'Total_HNT' : round(bal, 2),
+        'Total_USD' : round(usdBal, 2),
         
     }
     dataDict = {
             'Balance' : balanceDict,
-            'Hotspots' : rewardList
-        }
-    return dataDict
+            'Hotspots' : rewardList,
             
-# Main Function
-def main():
-    twentyfourHour = '-2%20day'
-    thirtyDays = '-30%20day'
-
-    # This is for getting the Hotspot Addr fromt the csv file
-    col_list = ["Hopstop Addr"]
-    df = pd.read_csv('HeliumData.csv', usecols=col_list)
-    data = df.values
-    addr = list(np.concatenate(data).flat)
-    
-    # Get the Host's Name from csv
-    col_list = ["Host Name"]
-    df = pd.read_csv('HeliumData.csv', usecols=col_list)
-    data = df.values
-    hostName = list(np.concatenate(data).flat)
-    
-    # Get the Hotspot Name in a list
-    col_list2 = ["Hotspot Name"]
-    df = pd.read_csv('HeliumData.csv', usecols=col_list2)
-    data = df.values
-    hotspotName = list(np.concatenate(data).flat)
-    
-    # This is for getting the Hotspot Addr fromt the csv file
-    col_list = ["Account Addr"]
-    df = pd.read_csv('HeliumData.csv', usecols=col_list)
-    data = df.values
-    accAddr = list(np.concatenate(data).flat)
-    
-    rewardList = get_rewards(addr, twentyfourHour, thirtyDays, hostName, hotspotName, accAddr)
-    
-    # put the result into a json
-    jsonList = json.dumps(rewardList, indent = 4)
-    # change to this for aws Lambda instead of print
-    # return{
-    #     'statusCode' : 200,
-    #     'body': jsonList
-    # }
-    print(jsonList)
-
-if __name__ == "__main__":
-    main()
+        }
+    dataList = [dataDict]
+    return dataList
