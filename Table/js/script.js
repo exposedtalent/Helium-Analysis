d1a0bdba38fc219e760a9a5f5514361ecbf3c659
Tanishq Mor <Tanishq.Mor@gmail.com>
2021-05-28 19:18:33 -0700

    updated

--- Table/js/script.js
+++ Table/js/script.js
@@ -1,40 +1,39 @@
-/* global $*/
-$(document).ready(function(){
-    $.getJSON("../data.json", function(data){
-    
-        let array = data['Hotspots'];
-        let dict = data['Balance'];
-    
-        buildBalTable(dict)
-        buildTable(array)
+$(document).ready(function () {
+    $.getJSON("https://heliumfrontend.s3.amazonaws.com/data.json", function (data) {
+        let array = data["Hotspots"];
+        let dict = data["Balance"];
 
-        $('th').on('click', function(){
-            var column = $(this).data('column')
-            var order = $(this).data('order')
-            console.log('Col in the function', column, order)
-        
-            if(order == 'desc'){
-                $(this).data('order', "asc")
-                array = array.sort((a,b) => a[column] > b[column]? 1 : -1)   
+        buildBalTable(dict);
+        buildTable(array);
+
+        $("th").on("click", function () {
+            var column = $(this).data("column");
+            var order = $(this).data("order");
+
+            if (order == "desc") {
+                $(this).data("order", "asc");
+                array = array.sort((a, b) => (a[column] > b[column] ? 1 : -1));
+            } else {
+                $(this).data("order", "asc");
+                array = array.sort((a, b) => (a[column] < b[column] ? 1 : -1));
             }
-            else{
-                $(this).data('order', "asc")
-                array = array.sort((a,b) => a[column] < b[column]? 1 : -1)
-            }
-            buildTable(array)
-        
+            buildTable(array);
+        });
+        // Adds the even listeners for the hotspots addr
+        $(document.body).on("click", "td[data-href]", function (){
+            $(this).text();
+            window.location.href = this.dataset.href
         })
-        
-    }).fail(function (){
+
+    }).fail(function () {
         console.log("An error has occurred.");
     });
 });
 
 
 
-
-function buildBalTable(data){
-    let table = document.getElementById('myBalTable')
+function buildBalTable(data) {
+    let table = document.getElementById("myBalTable");
     let row = `<tr>
         <td>${data.Hotspots_24H_HNT}</td>
         <td>${data.Hotspots_24H_USD}</td>
@@ -42,18 +41,19 @@ function buildBalTable(data){
         <td>${data.Hotspots_30D_USD}</td>
         <td>${data.Total_HNT}</td>
         <td>${data.Total_USD}</td>
-     </tr>`
-    table.innerHTML += row
-
+     </tr>`;
+    table.innerHTML += row;
 }
 
-function buildTable(data){
-    let table = document.getElementById('myTable')
-    table.innerHTML = ''
-    for (let i = 0; i < data.length; i++){
+function buildTable(data) {
+    let table = document.getElementById("myTable");
+    table.innerHTML = "";
+    base_url = "https://mto1ratv44.execute-api.us-east-2.amazonaws.com/default/invoke-HeliumRewards/";
+    console.log(base_url)
+    for (let i = 0; i < data.length; i++) {
         let row = `<tr>
             <td>${data[i].Hotspot_Owner}</td>
-            <td>${data[i].Hotspot_Address}</td>
+            <td data-href="${base_url}${data[i].Hotspot_Address}">${data[i].Hotspot_Address}</td>
             <td>${data[i].Hotspot_Name}</td>
             <td>${data[i].Hotspot_24H_HNT}</td>
             <td>${data[i].Hotspot_24H_USD}</td>
@@ -62,7 +62,7 @@ function buildTable(data){
             <td>${data[i].Hotspot_30D_USD}</td>
             <td>${data[i].Wallet_Balance}</td>
             <td>${data[i].Wallet_Balance_USD}</td>
-        </tr>`
-        table.innerHTML += row
+        </tr>`;
+        table.innerHTML += row;
     }
 }
