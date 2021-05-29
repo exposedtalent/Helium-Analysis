6658e722ea9526db43d544a0ea46dd0466242bc9
Tanishq Mor <Tanishq.Mor@gmail.com>
2021-05-29 00:22:03 -0700

    added Hotspot Name in the front end

--- AWS/awsAnalysisLambda.py
+++ AWS/awsAnalysisLambda.py
@@ -1,255 +1,234 @@
-import requests
+from datetime import datetime
 import pandas as pd
 import numpy as np
 import json
-
-rewardList = None
-
+import boto3
+import requests
 
 def lambda_handler(event, context):
-    # This is to figure out the time
-    twentyfourHour = '-2%20day'
-    thirtyDays = '-30%20day'
+    # gets the uri from the url
+    hotspots = []
+    path = event['path']
+    x = path.split("/")
+    hotspots.append(x[2])
 
-    # This is for getting the Hotspot Addr fromt the csv file
-    col_list = ["Hopstop Addr"]
-    df = pd.read_csv('HeliumData.csv', usecols=col_list)
-    data = df.values
-    addr = list(np.concatenate(data).flat)
-
-    # Get the Host's Name from csv
-    col_list = ["Host Name"]
-    df = pd.read_csv('HeliumData.csv', usecols=col_list)
-    data = df.values
-    hostName = list(np.concatenate(data).flat)
-
-    # Get the Hotspot Name in a list
-    col_list2 = ["Hotspot Name"]
-    df = pd.read_csv('HeliumData.csv', usecols=col_list2)
-    data = df.values
-    hotspotName = list(np.concatenate(data).flat)
-
-    # This is for getting the Hotspot Addr fromt the csv file
-    col_list = ["Account Addr"]
-    df = pd.read_csv('HeliumData.csv', usecols=col_list)
-    data = df.values
-    accAddr = list(np.concatenate(data).flat)
-    # Calling the get_rewards and putting the data into reward list
-    rewardsList = get_rewards(addr, twentyfourHour,thirtyDays, hostName, hotspotName, accAddr)
+    # This is the main program
+    pageNum = 2
+    # Turn True for for to print the avg reward scale 
+    # WARNING : ADDS A LOT MORE TIME TO EXECUTE
+    rewardScale = False
     
-    # Dynamically adding data from the varibles into a string used for js script
-    balanceHtmlDict = """let dict = %s"""%rewardsList['Balance']
-    # Dynamically adding data from the varibles into a string used for js script
-    hotspotsHtmlList = """\nlet array = %s"""%rewardsList['Hotspots']
-
-    # Top half of the html with inline css code. This creates a table and style it using css
-    topHtml = """<!DOCTYPE html><html ><head>
-      <meta charset="UTF-8">
-      <title>Wifi Mist</title>
-      <script src="https://s.codepen.io/assets/libs/modernizr.js" type="text/javascript"></script>
-      <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-      <link rel="stylesheet" href= "https://heliumfrontend.s3.amazonaws.com/style.css">
-    <style>
-    td[data-href]{
-    cursor: pointer;
-    }
-    </style>
-    </head>
-    
-    <body style="background-color:lightblue;">
-      <h1>Balance</h1>
-     <div style="overflow-x:auto;">
-    <table class="rwd-table">
-      <tr>
-        <th>HNT Price</th>
-        <th>Hotspot 24H HNT</th>
-        <th>Hotspot 24H USD</th>
-        <th >Hotspot 30D HNT</th>
-        <th >Hotspot 30D USD</th>
-        <th >Total HNT</th>
-        <th >Total USD</th>
-      </tr>
-      <tbody id="myBalTable">
-      
-    </table>
-    </div>
-    <h1>Hotspots Information</h1>
-    <div style="overflow-x:auto;">
-    <table class="rwd-table">
-    
-        <th data-column='HotspotOwner' data-order='desc'>Hotspot Owner &#9650</th>
-        <th data-column='HotspotAddress' data-order='desc'>Hotspot Address &#9650</th>
-        <th data-column='HotspotName' data-order='desc'>Hotspot Name &#9650</th>
-        <th data-column='Hotspot24HHNT' data-order='desc'>Hotspot 24H HNT &#9650</th>
-        <th data-column='Hotspot24HUSD' data-order='desc'>Hotspot 24H USD &#9650</th>
-        <th data-column='Change' data-order='desc'>24H Change &#9650</th>
-        <th data-column='Hotspot30DHNT' data-order='desc'>Hotspot 30D HNT &#9650</th>
-        <th data-column='Hotspot30DUSD' data-order='desc'>Hotspot 30D USD &#9650</th>
-        <th data-column='WalletBalance' data-order='desc'>Wallet Balance &#9650</th>
-        <th data-column='WalletBalanceUSD' data-order='desc'>Wallet Balance USD &#9650</th>
+    data = analyze_hotspot(hotspots[0], pageNum, rewardScale)
+    df = pd.DataFrame(data)
+    to_csv(df.to_csv())
+    sortCSV('s3://helium-analysis-data/hotspotData.csv')
+    analysis = summary('s3://helium-analysis-data/Sorted_hotspotData.csv', rewardScale)
+    jsonList = json.dumps(analysis, indent = 4)
+    WitnesseshtmlList = """let array = %s;"""%analysis['Witnesses']
+    hotspotName = """\nlet hotspotName = "%s";"""%hotspots[0]
+    htmlTop = """
+        <!DOCTYPE html>
+        <html >
+        <head>
+        <meta charset="UTF-8">
+        <title>Wifi Mist</title>
+        
+        <script src="https://s.codepen.io/assets/libs/modernizr.js" type="text/javascript"></scrip>
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+        <link rel="stylesheet" href= "https://heliumfrontend.s3.amazonaws.com/style.css">
+        </head>
+        
+        <body style="background-color:lightblue;">
+        <h1>Witnesses</h1>
+        <div style="overflow-x:auto;">
+        <table class="rwd-table">
+        <tr>
+        <th>Hotspot Name </th>
+        <th>1 Witnesses</th>
+        <th>2 Witnesses</th>
+        <th>3 Witnesses</th>
+        <th>4 Witnesses</th>
+        <th>5 Witnesses and up</th>
+        <th>Total Witnesses from 1-4</th>
+        <th>Total Witnesses</th>
         </tr>
-    
-      <tbody id="myTable">
-     </table>
-    </div>
+        <tbody id="myTable">
+        
+        </table>
+        <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
         <script>
     """
-    # This is the bottom of the html with js script inline
-    bottomHtml = """
-    $('th').on('click', function(){
-                var column = $(this).data('column')
-                var order = $(this).data('order')
-                console.log('Col in the function', column, order)
-            
-                if(order == 'desc'){
-                    $(this).data('order', "asc")
-                    array = array.sort((a,b) => a[column] > b[column]? 1 : -1)   
-                }
-                else{
-                    $(this).data('order', "asc")
-                    array = array.sort((a,b) => a[column] < b[column]? 1 : -1)
-                }
-                buildTable(array)
-            
-            })
-     $(document.body).on("click", "td[data-href]", function (){
-            $(this).text();
-            window.location.href = this.dataset.href
-        })
-    buildTable(array)
-    buildBalTable(dict)
-    
-    
-    function buildBalTable(data){
-        let table = document.getElementById("myBalTable")
-        let row = `<tr>
-            <td>${data.HNT_Price}</td>
-            <td>${data.Hotspots_24H_HNT}</td>
-            <td>${data.Hotspots_24H_USD}</td>
-            <td>${data.Hotspots_30D_HNT}</td>
-            <td>${data.Hotspots_30D_USD}</td>
-            <td>${data.Total_HNT}</td>
-            <td>${data.Total_USD}</td>
-         </tr>`
-        table.innerHTML += row
-    
-    }
-    
-    function buildTable(data){
-        let table = document.getElementById("myTable")
-        table.innerHTML = ""
-        base_url = "https://gfz4azqik2.execute-api.us-east-2.amazonaws.com/default/invoke-HeliumRewards/";        
-        for (let i = 0; i < data.length; i++){
+    htmlBottom = """
+        buildTable(array, hotspotName);
+        
+        function buildTable(data, hotspotName) {
+            let table = document.getElementById("myTable");
+
             let row = `<tr>
-                <td>${data[i].Hotspot_Owner}</td>
-                <td data-href="${base_url}${data[i].Hotspot_Address}">${data[i].Hotspot_Address}</td>
-                <td>${data[i].Hotspot_Name}</td>
-                <td>${data[i].Hotspot_24H_HNT}</td>
-                <td>${data[i].Hotspot_24H_USD}</td>
-                <td>${data[i].Change_24H}</td>
-                <td>${data[i].Hotspot_30D_HNT}</td>
-                <td>${data[i].Hotspot_30D_USD}</td>
-                <td>${data[i].Wallet_Balance}</td>
-                <td>${data[i].Wallet_Balance_USD}</td>
-            </tr>`
-            table.innerHTML += row
+                <td>${hotspotName}</td>
+                <td>${data.Witnesses_1}</td>
+                <td>${data.Witnesses_2}</td>
+                <td>${data.Witnesses_3}</td>
+                <td>${data.Witnesses_4}</td>
+                <td>${data.Witnesses_5_and_up}</td>
+                <td>${data.Total_Witnesses_from_1_to_4}</td>
+                <td>${data.Total_Witnesses}</td>
+            </tr>`;
+            table.innerHTML += row;
+    
         }
-    }
-      </script>
-    
-    
-    </body>
-    </html>
+        </script>
+        </body>
+        </html>
     """
-    # Finally put together the differernt html strings into one to be returned
-    finalHtml = topHtml + balanceHtmlDict + hotspotsHtmlList + bottomHtml
-
-    # returns the final html string and that is run on the client side
+    
+    finalhtml = htmlTop + WitnesseshtmlList + hotspotName + htmlBottom
     return{
-        # "statusCode": 200,
+        "statusCode": 200,
         "headers": {'Content-Type': 'text/html'},
-        "body": finalHtml
+        "body": finalhtml
     }
+    
+def to_csv(data):
+    count = 0 
+    if count == 0:
+        bucket_name = "helium-analysis-data"
+        file_name = "hotspotData" + ".csv"
+        s3_path = file_name
+    
+        s3 = boto3.resource("s3")
+        s3.Bucket(bucket_name).put_object(Key=s3_path, Body=data)
+        count += 1
+    if count == 1:
+        bucket_name = "helium-analysis-data"
+        file_name = "Sorted_hotspotData" + ".csv"
+        s3_path = file_name
+        s3 = boto3.resource("s3")
+        s3.Bucket(bucket_name).put_object(Key=s3_path, Body=data)
 
-
-# Function to get the reward summary of the given hotspots
-def get_rewards(hotspot, twentyfourHour, thirtyDays, hostName, hotspotName, accAddr):
-    # initlize the various lists
-    rewardList = []
-    total24hrs = []
-    total30days = []
-    rewardChange = []
-    balanceList = []
-
-    # for loop for getting the reward summary from the Helium API
-    for i in range(len(hotspot)):
-        # URL for the 24 hours
-        url = 'https://api.helium.io/v1/hotspots/' + hotspot[i] + '/rewards/sum?min_time=' + twentyfourHour + '&bucket=day'
+# Function to analyzer the data from the activity tab of the Helium API 
+def analyze_hotspot(hotspot, pagecount, rewardScale):
+    tableList = []
+    output={"data": []}    
+    base_url='https://api.helium.io/v1/hotspots/' + hotspot + '/activity'
+    url = base_url
+    # While loop to go thrrough the num of pages specified by the user and get all the data from the Helium api
+    while pagecount:
         response = requests.get(url)
         new_data = response.json()
-        reward24hrs = new_data['data'][0]['total']
-        reward2day = new_data['data'][1]['total']
-        change = (round((reward24hrs - reward2day) / reward2day * 100, 2))
-        rewardChange.append(change)
-        total24hrs.append(reward24hrs)
+        output['data'].extend(new_data['data']) 
+        cursor=new_data['cursor']
+        url=base_url+'?cursor='+cursor
+        pagecount -= 1
+        # For loop to parse the data coming from the json file
+    for i in output['data']:
+        if (i['type'] == "poc_receipts_v1") and (i['path'][0]['challengee'] != hotspot):
+            timestamp = datetime.fromtimestamp(i['time']).strftime("%Y-%m-%d-%I:%M:%S")
+            challengee = i['path'][0]['challengee']
+            # Get the reward scale of the challengee
+            if rewardScale:
+                reward_scale = get_reward_scale(challengee)
+            street = i['path'][0]['geocode']['short_street']
+            city = i['path'][0]['geocode']['short_city']
+            witnesses = len(i['path'][0]['witnesses'])
+            
+            # Create a dict of the data
+            if(witnesses != 0 and rewardScale):
+                alltheData = {
+                    'TimeStamp' : timestamp,
+                    'Challengee' : challengee,
+                    'reward_scale': reward_scale,
+                    'Street'    : street,
+                    'City'      : city,
+                    'Witnesses' : witnesses
+                }
+            if(witnesses != 0):
+                alltheData = {
+                    'TimeStamp' : timestamp,
+                    'Challengee' : challengee,
+                    'Street'    : street,
+                    'City'      : city,
+                    'Witnesses' : witnesses
+                }
+                # Append all of it to the tableList
+                tableList.append(alltheData)
+    return tableList
 
-        # URL for the 30 days
-        url = 'https://api.helium.io/v1/hotspots/' + hotspot[i] + '/rewards/sum?min_time=' + thirtyDays
-        response = requests.get(url)
-        new_data = response.json()
-        reward30days = new_data['data']['total']
-        total30days.append(reward30days)
+def summary(file,rewardScale):
+    finalDict = {}
+    hotspotNameDict = {}
+    witnesseDict = {}
+    # Looks at only the Witnesses col and prints the num of Witnesses 
+    col_list = ["Witnesses"]
+    df = pd.read_csv(file, usecols=col_list)
+    data = df.values
+    
+    if rewardScale:
+        # Looks at only the Reward Scale col and store it in a list
+        col_list2 = ["reward_scale"]
+        df2 = pd.read_csv(file, usecols=col_list2)
+        rewardScale = df2.values
+        rs = list(np.concatenate(rewardScale).flat)
+        
+    # Looks at only the Challengee col and store it in a list
+    new_col3 = ["Challengee"]
+    df3 = pd.read_csv(file, usecols=new_col3)
+    Challengee = df3
 
-    # Binance API to get the current rate
-    url = 'https://api.binance.com/api/v3/ticker/price?symbol=HNTUSDT'
-    response = requests.get(url)
-    new_data = response.json()
-    price = float(new_data['price'])
-
-    # For loop to get the account balance of the user
-    for i in range(len(accAddr)):
-        url = 'https://api.helium.io/v1/accounts/' + accAddr[i] + '/stats'
-        response = requests.get(url)
-        new_data = response.json()
-        balance = new_data['data']['last_day'][0]['balance']
-        balanceList.append(balance)
-
-    # Calculations
-    bal = sum(balanceList) / 100000000
-    usdBal = bal * price
-    # Loop to get the data into a dict that is appened to a list
-    for i in range(len(hotspot)):
-        # Put eveything into a dict
-        rewardDict = {
-            'Hotspot_Owner': hostName[i],
-            'Hotspot_Address': hotspot[i],
-            'Hotspot_Name': hotspotName[i],
-            'Hotspot_24H_HNT': round(total24hrs[i], 2),
-            'Hotspot_24H_USD': round(total24hrs[i] * price, 2),
-            'Change_24H': rewardChange[i],
-            'Hotspot_30D_HNT': round(total30days[i], 2),
-            'Hotspot_30D_USD': round(total30days[i] * price, 2),
-            'Wallet_Balance': round(balanceList[i] / 100000000, 2),
-            'Wallet_Balance_USD': round((balanceList[i] / 100000000) * price, 2),
-
+    # using the numpy lib we find the number of times this router has seen 1,2,3,4,5 or more witnesses for a challenge
+    occurOne = np.count_nonzero(data == 1)  
+    occurTwo = np.count_nonzero(data == 2)
+    occurThree = np.count_nonzero(data == 3)
+    occurFour = np.count_nonzero(data == 4)
+    occurMore = np.count_nonzero(data >= 5)
+    onetofour = occurOne + occurTwo + occurThree + occurFour 
+    if rewardScale:
+        print(
+            "============= Witnesses =============\n"
+            " 1 Witnesses : ", occurOne, "\tAVG Reward Scale :", round(avg(rs[0:occurOne]),3),"\n", 
+            "2 Witnesses : ", occurTwo, "\tAVG Reward Scale :", round(avg(rs[occurOne:occurOne+occurTwo]),3),"\n",
+            "3 Witnesses : ", occurThree, "\tAVG Reward Scale :", round(avg(rs[occurOne+occurTwo:occurOne+occurTwo+occurThree]),3),"\n",
+            "4 Witnesses : ", occurFour, "\tAVG Reward Scale :", round(avg(rs[occurOne+occurTwo+occurThree:occurOne+occurTwo+occurThree+occurFour]),3),"\n",
+            "More than 5 Witnesses :", occurMore, "\n\n",
+            "Total Witnesses from 1-4 : ", onetofour, "\n",
+            "Total Witnesses : ", onetofour + occurMore, "\n",
+            )
+    else :
+        witnesseDict = {
+            "Witnesses_1" : occurOne,
+            "Witnesses_2" : occurTwo,
+            "Witnesses_3" : occurThree,
+            "Witnesses_4" : occurFour,
+            "Witnesses_5_and_up" : occurMore,
+            "Total_Witnesses_from_1_to_4" : onetofour, 
+            "Total_Witnesses" : onetofour + occurMore
+            
         }
-        rewardList.append(rewardDict)
-    # Dict for the balance part of the json
-    balanceDict = {
-        'HNT_Price': round(price, 2),
-        'Hotspots_24H_HNT': round(sum(total24hrs), 2),
-        'Hotspots_24H_USD': round(sum(total24hrs) * price, 2),
-        'Hotspots_30D_HNT': round(sum(total30days), 2),
-        'Hotspots_30D_USD': round(sum(total30days) * price, 2),
-        'Total_HNT': round(bal, 2),
-        'Total_USD': round(usdBal, 2),
-
+    hotspotNameDict = {
+        "Hotspots_with_1_Witnesses" : remove_dup(Challengee[0:occurOne].values), 
+        "Hotspots_with_2_Witnesses" : remove_dup(Challengee[occurOne:occurOne+occurTwo].values),
+        "Hotspots_with_3_Witnesses" : remove_dup(Challengee[occurOne+occurTwo:occurOne+occurTwo+occurThree].values), 
+        "Hotspots_with_4_Witnesses" : remove_dup(Challengee[occurOne+occurTwo+occurThree:occurOne+occurTwo+occurThree+occurFour].values)
+        
     }
-    # Put the two different dicts into one big one for the json
-    dataDict = {
-        'Balance': balanceDict,
-        'Hotspots': rewardList,
-
+    
+    finalDict = {
+        "Witnesses": witnesseDict,
+        "HotspotName": hotspotNameDict
     }
-    # return
-    return dataDict
+    return finalDict
+
+def remove_dup(x):
+    tuple_line = [tuple(pt) for pt in x]                            # convert list of list into list of tuple
+    tuple_new_line = sorted(set(tuple_line),key=tuple_line.index)   # remove duplicated element
+    new_line = [list(t) for t in tuple_new_line]                    # convert list of tuple into list of list
+    return new_line
+
+def sortCSV(file):
+
+    # after the file is written we need to sort the csv file using pandas library 
+    df = pd.read_csv('s3://helium-analysis-data/hotspotData.csv')
+    sorted_df = df.sort_values(by=["Witnesses"], ascending=True)
+    to_csv(sorted_df.to_csv())
+
+
