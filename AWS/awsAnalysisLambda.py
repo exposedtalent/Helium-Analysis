4ec73577bd2b5ac909a0994855a1f8e86a361af9
Tanishq Mor <Tanishq.Mor@gmail.com>
2021-06-22 15:08:14 -0700

    updated with working dynamodb querying

--- AWS/awsAnalysisLambda.py
+++ AWS/awsAnalysisLambda.py
@@ -1,30 +1,63 @@
-from datetime import datetime
+from datetime import date, timedelta
 import pandas as pd
 import numpy as np
 import json
 import boto3
+from boto3.dynamodb.conditions import Key, And, Attr, Between
+
 import requests
 
 def lambda_handler(event, context):
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table("Hotspotdetail")
+    
+    todayDate = date.today()
+    td = timedelta(1)
+    d = todayDate + td
+    upperDate = '%s'%d
+    td = timedelta(3)
+    d = todayDate - td
+    lowerDate = '%s'%d
+    
     # gets the uri from the url
     hotspots = []
     path = event['path']
+    # path = "/invoke/112XTwrpTBHjg4M1DWsLTcqsfJVZCPCYW2vNPJV7cZkpRg3JiKEg"
     x = path.split("/")
     hotspots.append(x[2])
 
-    # This is the main program
-    pageNum = 2
-    # Turn True for for to print the avg reward scale 
-    # WARNING : ADDS A LOT MORE TIME TO EXECUTE
-    rewardScale = False
-    
-    data = analyze_hotspot(hotspots[0], pageNum, rewardScale)
-    df = pd.DataFrame(data)
-    to_csv(df.to_csv())
-    sortCSV('s3://helium-analysis-data/hotspotData.csv')
-    analysis = summary('s3://helium-analysis-data/Sorted_hotspotData.csv', rewardScale)
-    jsonList = json.dumps(analysis, indent = 4)
-    WitnesseshtmlList = """let array = %s;"""%analysis['Witnesses']
+    response = table.query(
+    #   FilterExpression= Attr("Witnesses").between(1,5),
+       KeyConditionExpression=Key('Hotspot').eq('112XTwrpTBHjg4M1DWsLTcqsfJVZCPCYW2vNPJV7cZkpRg3JiKEg') & Key('WitnesseTime').between(lowerDate, upperDate)
+    )
+
+    occurOne = occurTwo = occurThree = occurFour = occurMore = 0
+
+    for i in range(len(response['Items'])):
+        if(response['Items'][i]['Witnesses'] == 1):
+            occurOne = occurOne + 1
+        elif(response['Items'][i]['Witnesses'] == 2):
+            occurTwo = occurTwo + 1
+        elif(response['Items'][i]['Witnesses'] == 3):
+            occurThree = occurThree + 1
+        elif(response['Items'][i]['Witnesses'] == 4):
+            occurFour = occurFour + 1
+        else:
+            occurMore = occurMore + 1
+        
+    onetofour = occurOne +occurTwo + occurThree + occurFour
+    witnesseDict = {
+        "Witnesses_1" : occurOne,
+        "Witnesses_2" : occurTwo,
+        "Witnesses_3" : occurThree,
+        "Witnesses_4" : occurFour,
+        "Witnesses_5_and_up" : occurMore,
+        "Total_Witnesses_from_1_to_4" : onetofour, 
+        "Total_Witnesses" : onetofour + occurMore
+        
+    }
+    WitnesseshtmlList = """let array = %s;"""%witnesseDict
     hotspotName = """\nlet hotspotName = "%s";"""%hotspots[0]
     htmlTop = """
         <!DOCTYPE html>
@@ -33,32 +66,90 @@ def lambda_handler(event, context):
         <meta charset="UTF-8">
         <title>Wifi Mist</title>
         
-        <script src="https://s.codepen.io/assets/libs/modernizr.js" type="text/javascript"></scrip>
-        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+        <script src="https://code.highcharts.com/highcharts.js"></script>
+        <script src="https://code.highcharts.com/modules/exporting.js"></script>
+        <script src="https://code.highcharts.com/modules/export-data.js"></script>
+        <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.js" integrity="sha512-otOZr2EcknK9a5aa3BbMR9XOjYKtxxscwyRHN6zmdXuRfJ5uApkHB7cz1laWk2g8RKLzV9qv/fl3RPwfCuoxHQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         <link rel="stylesheet" href= "https://heliumfrontend.s3.amazonaws.com/style.css">
+
         </head>
         
-        <body style="background-color:lightblue;">
+        <body style="background-color:white;">
         <h1>Witnesses</h1>
         <div style="overflow-x:auto;">
         <table class="rwd-table">
-        <tr>
-        <th>Hotspot Name </th>
-        <th>1 Witnesses</th>
-        <th>2 Witnesses</th>
-        <th>3 Witnesses</th>
-        <th>4 Witnesses</th>
-        <th>5 Witnesses and up</th>
-        <th>Total Witnesses from 1-4</th>
-        <th>Total Witnesses</th>
-        </tr>
+            <tr>
+            <th>Hotspot Name </th>
+            <th>1 Witnesses</th>
+            <th>2 Witnesses</th>
+            <th>3 Witnesses</th>
+            <th>4 Witnesses</th>
+            <th>5 Witnesses and up</th>
+            <th>Total Witnesses from 1-4</th>
+            <th>Total Witnesses</th>
+            </tr>
         <tbody id="myTable">
         
+        
         </table>
-        <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+        </div>
+        <script src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+        <h1> 24 Hrs & 30 Day Rewards </h1>
+        <figure class="highcharts-figure">
+          <div id="con7day" ></div>
+          </figure>
+          <figure class="highcharts-figure">
+          <div id="con30day" ></div>
+          
+        </figure>
         <script>
     """
     htmlBottom = """
+        let url30day = 'https://api.helium.io/v1/hotspots/' + hotspotName + '/rewards/sum?min_time=-30%20day&bucket=day'
+        console.log(url30day);
+        let url1day = 'https://api.helium.io/v1/hotspots/' + hotspotName + '/rewards/sum?min_time=-1%20day&bucket=hour'
+        let totalArray30day = []
+        let timeArray30day = []
+        let list30Day =[]
+        let totalArray1day = []
+        let timeArray1day = []
+        let list1Day =[]
+        axios.get(url30day)
+            .then(response => {
+                var count = 0;
+                for (let i = response.data.data.length - 1; i >= 0; i--) {
+                    var reward = response.data.data[i]['total'];
+                    var data = response.data.data[i]['timestamp'];
+                    var time = data.split("T")
+                    totalArray30day.push(reward)
+                    timeArray30day.push(time[0])
+                    list30Day.push([timeArray30day[count],totalArray30day[count]])
+                    count++;
+                }
+                buildGraph2(list30Day)
+                
+            }, error => {
+                console.log(error);
+            })
+        // for 7 days
+        axios.get(url1day)
+            .then(response => {
+                var count = 0;
+                for (let i = response.data.data.length - 1; i >= 0; i--) {
+                    var reward = response.data.data[i]['total'];
+                    var time = response.data.data[i]['timestamp'];
+                    totalArray1day.push(reward)
+                    timeArray1day.push(time)
+                    list1Day.push([timeArray1day[count], totalArray1day[count]])
+                    count++;
+                }
+                
+                buildGraph(list1Day)
+            }, error => {
+                console.log(error);
+            })
+        
         buildTable(array, hotspotName);
         
         function buildTable(data, hotspotName) {
@@ -77,6 +168,104 @@ def lambda_handler(event, context):
             table.innerHTML += row;
     
         }
+    function buildGraph(list7Day) {
+        Highcharts.chart('con7day', {
+            chart: {
+              type: 'column'
+            },
+            title: {
+              text: '24 Hour Reward'
+            },
+            
+            xAxis: {
+              type: 'category',
+              labels: {
+                rotation: -45,
+                style: {
+                  fontSize: '13px',
+                  fontFamily: 'Verdana, sans-serif'
+                }
+              }
+            },
+            yAxis: {
+              min: 0,
+              title: {
+                text: 'HNT'
+              }
+            },
+            legend: {
+              enabled: false
+            },
+            tooltip: {
+              pointFormat: 'HNT: <b>{point.y:.1f} HNT</b>'
+            },
+            series: [{
+              name: 'HNT',
+              data: list7Day,
+              dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 20, // 10 pixels down from the top
+                style: {
+                  fontSize: '13px',
+                  fontFamily: 'Verdana, sans-serif'
+                }
+              }
+            }]
+          });
+    }
+    
+    function buildGraph2(list30Day) {
+        Highcharts.chart('con30day', {
+            chart: {
+              type: 'column'
+            },
+            title: {
+              text: '30 Day Reward'
+            },
+            xAxis: {
+              type: 'category',
+              labels: {
+                rotation: -45,
+                style: {
+                  fontSize: '13px',
+                  fontFamily: 'Verdana, sans-serif'
+                }
+              }
+            },
+            yAxis: {
+              min: 0,
+              title: {
+                text: 'HNT'
+              }
+            },
+            legend: {
+              enabled: false
+            },
+            tooltip: {
+              pointFormat: 'HNT: <b>{point.y:.1f} HNT</b>'
+            },
+            series: [{
+              name: 'HNT',
+              data: list30Day,
+              dataLabels: {
+                enabled: true,
+                rotation: -90,
+                color: '#FFFFFF',
+                align: 'right',
+                format: '{point.y:.1f}', // one decimal
+                y: 5, // 10 pixels down from the top
+                style: {
+                  fontSize: '10px',
+                  fontFamily: 'Verdana, sans-serif'
+                }
+              }
+            }]
+          });
+    }
         </script>
         </body>
         </html>
@@ -89,124 +278,3 @@ def lambda_handler(event, context):
         "body": finalhtml
     }
     
-def to_csv(data):
-    count = 0 
-    if count == 0:
-        bucket_name = "helium-analysis-data"
-        file_name = "hotspotData" + ".csv"
-        s3_path = file_name
-    
-        s3 = boto3.resource("s3")
-        s3.Bucket(bucket_name).put_object(Key=s3_path, Body=data)
-        count += 1
-    if count == 1:
-        bucket_name = "helium-analysis-data"
-        file_name = "Sorted_hotspotData" + ".csv"
-        s3_path = file_name
-        s3 = boto3.resource("s3")
-        s3.Bucket(bucket_name).put_object(Key=s3_path, Body=data)
-
-# Function to analyzer the data from the activity tab of the Helium API 
-def analyze_hotspot(hotspot, pagecount, rewardScale):
-    tableList = []
-    output={"data": []}    
-    base_url='https://api.helium.io/v1/hotspots/' + hotspot + '/activity'
-    url = base_url
-    # While loop to go thrrough the num of pages specified by the user and get all the data from the Helium api
-    while pagecount:
-        response = requests.get(url)
-        new_data = response.json()
-        output['data'].extend(new_data['data']) 
-        cursor=new_data['cursor']
-        url=base_url+'?cursor='+cursor
-        pagecount -= 1
-        # For loop to parse the data coming from the json file
-    for i in output['data']:
-        if (i['type'] == "poc_receipts_v1") and (i['path'][0]['challengee'] != hotspot):
-            timestamp = datetime.fromtimestamp(i['time']).strftime("%Y-%m-%d-%I:%M:%S")
-            challengee = i['path'][0]['challengee']
-            # Get the reward scale of the challengee
-            street = i['path'][0]['geocode']['short_street']
-            city = i['path'][0]['geocode']['short_city']
-            witnesses = len(i['path'][0]['witnesses'])
-            
-            # Create a dict of the data
-            if(witnesses != 0):
-                alltheData = {
-                    'TimeStamp' : timestamp,
-                    'Challengee' : challengee,
-                    'Street'    : street,
-                    'City'      : city,
-                    'Witnesses' : witnesses
-                }
-                # Append all of it to the tableList
-                tableList.append(alltheData)
-    return tableList
-
-def summary(file,rewardScale):
-    finalDict = {}
-    hotspotNameDict = {}
-    witnesseDict = {}
-    # Looks at only the Witnesses col and prints the num of Witnesses 
-    col_list = ["Witnesses"]
-    df = pd.read_csv(file, usecols=col_list)
-    data = df.values
-    
-    if rewardScale:
-        # Looks at only the Reward Scale col and store it in a list
-        col_list2 = ["reward_scale"]
-        df2 = pd.read_csv(file, usecols=col_list2)
-        rewardScale = df2.values
-        rs = list(np.concatenate(rewardScale).flat)
-        
-    # Looks at only the Challengee col and store it in a list
-    new_col3 = ["Challengee"]
-    df3 = pd.read_csv(file, usecols=new_col3)
-    Challengee = df3
-
-    # using the numpy lib we find the number of times this router has seen 1,2,3,4,5 or more witnesses for a challenge
-    occurOne = np.count_nonzero(data == 1)  
-    occurTwo = np.count_nonzero(data == 2)
-    occurThree = np.count_nonzero(data == 3)
-    occurFour = np.count_nonzero(data == 4)
-    occurMore = np.count_nonzero(data >= 5)
-    onetofour = occurOne + occurTwo + occurThree + occurFour 
-    
-    witnesseDict = {
-        "Witnesses_1" : occurOne,
-        "Witnesses_2" : occurTwo,
-        "Witnesses_3" : occurThree,
-        "Witnesses_4" : occurFour,
-        "Witnesses_5_and_up" : occurMore,
-        "Total_Witnesses_from_1_to_4" : onetofour, 
-        "Total_Witnesses" : onetofour + occurMore
-        
-    }
-    hotspotNameDict = {
-        "Hotspots_with_1_Witnesses" : remove_dup(Challengee[0:occurOne].values), 
-        "Hotspots_with_2_Witnesses" : remove_dup(Challengee[occurOne:occurOne+occurTwo].values),
-        "Hotspots_with_3_Witnesses" : remove_dup(Challengee[occurOne+occurTwo:occurOne+occurTwo+occurThree].values), 
-        "Hotspots_with_4_Witnesses" : remove_dup(Challengee[occurOne+occurTwo+occurThree:occurOne+occurTwo+occurThree+occurFour].values)
-        
-    }
-    
-    finalDict = {
-        "Witnesses": witnesseDict,
-        "HotspotName": hotspotNameDict
-    }
-    return finalDict
-
-def remove_dup(x):
-    tuple_line = [tuple(pt) for pt in x]                            # convert list of list into list of tuple
-    tuple_new_line = sorted(set(tuple_line),key=tuple_line.index)   # remove duplicated element
-    new_line = [list(t) for t in tuple_new_line]                    # convert list of tuple into list of list
-    return new_line
-
-def sortCSV(file):
-
-    # after the file is written we need to sort the csv file using pandas library 
-    df = pd.read_csv('s3://helium-analysis-data/hotspotData.csv')
-    sorted_df = df.sort_values(by=["Witnesses"], ascending=True)
-    to_csv(sorted_df.to_csv())
-
-
